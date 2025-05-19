ad7d7f8fd4fa162fffafffc1aeb3b0d1798497a0
Ryan <ryansilvadeoliveira53@gmail.com>
2025-05-18 21:27:36 -0300

    first commit

--- src/main/java/Jogos.java
+++ src/main/java/Jogos.java
@@ -1,5 +1,5 @@
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
@@ -7,16 +7,31 @@ import java.time.LocalDateTime;
 public class Jogos {
 
     @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column (name = " id_jogo")
     private String idJogo;
 
+    @Column (name = "nomedojogo", length = 100, nullable = false, unique = false)
     private String nomeDoJogo;
+
+    @Column (name = "desenvolvedores")
     private String desenvolvedores;
+
+    @Column (name = "ano_de_lancamento")
     private LocalDate anoDeLancamento;
+
+    @Column (name = "imagem_capa", length = 300, nullable = false)
     private String imagemCapa;
+
+    @Column (name = "duracao_media")
     private int duracaoMedia;
+
+    @Column (name = "descricao")
     private String descricao;
 
+    @Column (name = "created_at")
     private LocalDateTime created_at;
+    @Column (name = "updated_at")
     private LocalDateTime update_at;
 
 }
--- src/main/java/Usuario.java
+++ src/main/java/Usuario.java
@@ -1,4 +1,4 @@
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import jakarta.persistence.Id;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -7,16 +7,33 @@ import java.time.LocalDateTime;
 public class Usuario {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name = "id_usuario", length = 36)
     private String idUsuario;
 
+    @Column (name = "nome_completo", length = 90, nullable = false)
     private String nomeCompleto;
+
+    @Column (name = "email", length = 45, nullable = false, unique = true)
     private String email;
+
+    @Column (name = "celular", length = 20, nullable = false)
     private String celular;
+
+    @Column (name = "senha", length = 50, nullable = false, unique = true)
     private String senha;
+
+    @Column (name = "data_de_nascimento")
     private LocalDate dataDeNascimento;
+
+    @Column (name = "foto_usuario", length = 300)
     private String fotoUsuario;
+
+    @Column (name = "nickname", length = 45, nullable = false, unique = true)
     private String nickname;
 
+    @Column (name = "created_at")
     private LocalDateTime createdAt;
+    @Column (name = "updated_at")
     private LocalDateTime updatedAt;
 }
